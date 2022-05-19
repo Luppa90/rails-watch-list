0676ddbda04f2a14238a08cbae0634701b1ee1dc
Leith Essid <essidleith@gmail.com>
2022-05-19 22:25:17 +0200

    Finished database schema and models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+9.times do
+  Movie.create(title: Faker::Movie.title, overview: Faker::TvShows::BojackHorseman.quote, poster_url: Faker::Internet.url, rating: rand(0..10) )
+end
